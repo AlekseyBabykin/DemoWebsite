5f530168c10dbe29e6c2d81f7bdafb3c9d4229f8
Aleksey <a.babykiny@gmail.com>
2022-12-27 18:15:44 +0200

    navigation

--- src/main/java/com/myrkin/Blog/controllers/MainControllers.java
+++ src/main/java/com/myrkin/Blog/controllers/MainControllers.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainControllers {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home( Model model) {
         model.addAttribute("title", "Главная страница");
         return "home";
+    }
+    @GetMapping("/about")
+    public String about( Model model) {
+        model.addAttribute("title", "Страница про нас");
+        return "about";
     }
 
 }
